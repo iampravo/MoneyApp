2fa2df1620ad61d1e6343c2e53e000f794d11d9a
Pravinkumar Singh <pravinsinghkumar@gmail.com>
2019-04-17 23:07:27 +0530

    Update 5

--- src/test/java/integration/TransactionControllerITest.java
+++ src/test/java/integration/TransactionControllerITest.java
@@ -1,59 +1,58 @@
 package integration;
 
-import com.google.gson.Gson;
 import com.revolut.appsByPravin.MoneyApp.db.H2Database;
 import com.revolut.appsByPravin.MoneyApp.route.RouteController;
 import io.restassured.RestAssured;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import io.restassured.http.ContentType;
+import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Spark;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TransactionControllerITest {
     private static final Logger log = LoggerFactory.getLogger(TransactionControllerITest.class);
 
     @Test
-    public void testTransactionControllerEndpoint() {
+    public void testGetTransactions() {
         RestAssured.given()
                 .when()
                 .get("http://localhost:8182/moneyapp/v1/transactions")
                 .then()
                 .assertThat()
                 .statusCode(200);
+    }
 
-        Map<String, String> transaction = new HashMap<>();
-        transaction.put("fromAccountNumber", "1");
-        transaction.put("toAccountNumber", "3");
-        transaction.put("amount", "25");
-        transaction.put("currency", "USD");
+    @Test
+    public void testTransfer() {
+        String body = " { \n" +
+                "    \"fromAccountNumber\" = 1,\n" +
+                "    \"toAccountNumber\" = 3, \n" +
+                "    \"amount\" = 25, \n" +
+                "    \"currency\" = 'USD'\n" +
+                " }";
 
-        /*RestAssured.given()
-                .contentType("application/json")
-                .body(new Gson().toJson(transaction))
+        RestAssured.given()
+                .contentType(ContentType.TEXT)
+                .body(body)
                 .when()
-                .get("http://localhost:8182/moneyapp/v1/transfer")
+                .post("http://localhost:8182/moneyapp/v1/transfer")
                 .then()
                 .assertThat()
-                .statusCode(200);*/
+                .statusCode(200);
     }
 
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    public static void setUp() {
         configureAndStartWebServer();
     }
 
-    @After
-    public void tearDown() {
+    @AfterClass
+    public static void tearDown() {
         stopWebServer();
     }
 
-    private void configureAndStartWebServer() {
+    private static void configureAndStartWebServer() {
 
         log.info("Starting Application");
         Spark.port(8182);
@@ -63,8 +62,7 @@ public class TransactionControllerITest {
         log.info("Application has started successfully");
     }
 
-    private void stopWebServer() {
+    private static void stopWebServer() {
         Spark.stop();
     }
-
 }
